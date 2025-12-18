e6fb59818d17309823e5a5d1163664263dfd0eb7
sam <simonana@naver.com>
2025-12-18 15:24:44 +0900

    feat: s3 upload 구현

--- src/main/java/ktb/backend/service/S3Service.java
+++ src/main/java/ktb/backend/service/S3Service.java
@@ -2,51 +2,63 @@ package ktb.backend.service;
 
 import ktb.backend.S3Properties;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Request;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.time.Duration;
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class S3Service {
+    private static final String PET_IMAGE_FOLDER = "pet-images";
+    private static final String AI_GENERATED_IMAGE_FOLDER = "ai-generated-images";
+
     private final S3Properties s3Properties;
-    private final S3Presigner preSigner;
+    private final S3Client s3Client;
 
-    private String getPreSignedGetUrl(String folder, String filename) {
-        return preSigner
-                .presignGetObject(getObjectPresignRequest(folder, filename))
-                .url()
-                .toString();
+    public void uploadPetImage(Long imageId, MultipartFile file) {
+        try {
+            String key = buildKey(PET_IMAGE_FOLDER, imageId);
+
+            PutObjectRequest request = makePutObjectRequest(key, file);
+
+            s3Client.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private String getPresignedPutUrl(String folder, String filename) {
-        return preSigner
-                .presignPutObject(putObjectPresignRequest(folder, filename))
-                .url()
-                .toString();
+    public void uploadAiGeneratedImage(Long imageId, byte[] bytes) {
+        String key = buildKey(AI_GENERATED_IMAGE_FOLDER, imageId);
+
+        PutObjectRequest request = makePutBytesRequest(key);
+
+        s3Client.putObject(request, RequestBody.fromBytes(bytes));
     }
 
-    private GetObjectPresignRequest getObjectPresignRequest(String folder, String filename) {
-        return GetObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofMinutes(1))
-                .getObjectRequest(objectRequest ->
-                        objectRequest
-                                .bucket(s3Properties.bucket())
-                                .key(String.join("/", folder, filename)))
+    private String buildKey(String folder, Long imageId) {
+        return String.join("/", folder, imageId.toString());
+    }
+
+    private PutObjectRequest makePutObjectRequest(String key, MultipartFile file) {
+        return PutObjectRequest.builder()
+                .bucket(s3Properties.bucket())
+                .key(key)
+                .contentType(file.getContentType())
                 .build();
     }
 
-    private PutObjectPresignRequest putObjectPresignRequest(String folder, String filename) {
-        return PutObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofMinutes(1))
-                .putObjectRequest(objectRequest ->
-                        objectRequest
-                                .bucket(s3Properties.bucket())
-                                .key(String.join("/", folder, filename))
-                )
+    private PutObjectRequest makePutBytesRequest(String key) {
+        return PutObjectRequest.builder()
+                .bucket(s3Properties.bucket())
+                .key(key)
+                .contentType("image/jpeg")
                 .build();
     }
 }
