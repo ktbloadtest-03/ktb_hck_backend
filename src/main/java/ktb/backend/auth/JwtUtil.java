c22d1adc0546fd0d6d00d5dfc40cb661ce5b1da6
dokkang01 <kimd02.dev@gmail.com>
2025-12-19 03:29:23 +0900

    fix: resolved conflicts

--- src/main/java/ktb/backend/auth/JwtUtil.java
+++ src/main/java/ktb/backend/auth/JwtUtil.java
@@ -1,5 +1,10 @@
 package ktb.backend.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
